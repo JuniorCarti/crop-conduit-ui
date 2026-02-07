4f306e6baffce1ca3a7e6ee3d22257f56976f07d
JuniorCarti <ridgejunior204@gmail.com>
2026-02-07 23:15:26 +0300

    chore: remove sam build artifacts from repository

--- backend/.aws-sam/build/ListPostsFunction/src/lib/dynamo.js
+++
@@ -1,9 +0,0 @@
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
-
-const client = new DynamoDBClient({});
-const dynamo = DynamoDBDocumentClient.from(client, {
-  marshallOptions: { removeUndefinedValues: true }
-});
-
-module.exports = { dynamo };
