2a5b28746bf0c70292b3aa09803a8e428229deed
JuniorCarti <ridgejunior204@gmail.com>
2026-05-13 22:07:26 +0300

    test(mobile): update smoke test for AgriSmartApp

--- mobile/flutter_app/test/widget_test.dart
+++ mobile/flutter_app/test/widget_test.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:crop_conduit_flutter/main.dart';
 
 void main() {
-  testWidgets('App boots to splash screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const CropConduitApp());
-    expect(find.text('Crop Conduit'), findsOneWidget);
+  testWidgets('AgriSmart app smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const AgriSmartApp());
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
